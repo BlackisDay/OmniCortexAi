8a286f04f8a6415a7ed54bf78685f03a435733f2
Victor Hernandez <141363547+BlackisDay@users.noreply.github.com>
2026-03-25 04:59:36 -0400

    Delete apps directory

--- apps/mobile/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
-
-export const api = axios.create({
-  baseURL: API_URL,
-  headers: { "Content-Type": "application/json" },
-});
--- apps/web/src/components/ui/index.ts
+++
@@ -1,6 +0,0 @@
-export { Loader } from "./Loader";
-export { Card } from "./Card";
-export { default as Button } from "./Button";
-export { Input } from "./Input";
-export { Modal } from "./Modal";
-export { Table } from "./Table";
